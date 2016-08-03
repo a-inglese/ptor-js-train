c286592bc6224e89b4b9fb262ff35f2755cc4778
Zeque <euhrig@despegar.com>
2016-08-03 10:38:38 -0300

    local dependencies

--- spec.js
+++ spec.js
@@ -1,7 +1,13 @@
-describe('Protractor Demo App', function() {
+describe('Protractor Demo App pass', function() {
   it('should have a title', function() {
-    browser.get('http://www.despegar.com.ar/paquetes');
+    browser.get('http://www.despegar.com.ar/cp/shop/search/C982/C31684/2016-08-26/2016-09-02/2/0/0/NA/2016-08-26/2016-09-02/2');
+    expect(browser.getTitle()).toEqual('Despegar.com - Productos combinados');
+  });
+});
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
+describe('Protractor Demo App Fail', function() {
+  it('should have a title', function() {
+    browser.get('http://www.despegar.com.ar/cp/shop/search/C982/C31684/2016-08-26/2016-09-02/2/0/0/NA/2016-08-26/2016-09-02/2');
+    expect(browser.getTitle()).toEqual('Paquetes Turisticos, reserva tu Paquete ahora | Despegar.com');
   });
 });
