3565babd633ceb650336be60f966a3a30964d8cc
Zeque <euhrig@despegar.com>
2016-08-04 12:30:34 -0300

    multibrowser@

--- conf.js
+++ conf.js
@@ -1,6 +1,10 @@
 exports.config = {
   framework: 'jasmine',
   directConnect:true,
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+  specs: ['spec.js'],
+  multiCapabilities: [{
+   'browserName': 'chrome'
+   }, {
+  'browserName': 'firefox'
+  }]
 }
--- spec.js
+++ spec.js
@@ -5,9 +5,9 @@ describe('Protractor Demo App pass', function() {
   });
 });
 
-describe('Protractor Demo App Fail', function() {
-  it('should have a title', function() {
-    browser.get('http://www.despegar.com.ar/cp/shop/search/C982/C31684/2016-08-26/2016-09-02/2/0/0/NA/2016-08-26/2016-09-02/2');
-    expect(browser.getTitle()).toEqual('Paquetes Turisticos, reserva tu Paquete ahora | Despegar.com');
-  });
-});
+//describe('Protractor Demo App Fail', function() {
+//  it('should have a title', function() {
+//    browser.get('http://www.despegar.com.ar/cp/shop/search/C982/C31684/2016-08-26/2016-09-02/2/0/0/NA/2016-08-26/2016-09-02/2');
+//    expect(browser.getTitle()).toEqual('Paquetes Turisticos, reserva tu Paquete ahora | Despegar.com');
+//  });
+//});
