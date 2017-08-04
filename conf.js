7a92f64280c9dc3b88579d3fb2add5eaeb8e51db
Agustin Inglese <ainglese@despegar.com>
2017-08-04 11:34:09 -0300

    Update conf.js

--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec.js'],
   onPrepare: function(){
-    var url = 'http://www.despegar.com.ar/cp/shop/search/C982/C31684/2017-01-12/2017-01-15/2/0/0/NA/2017-01-12/2017-01-15/2';
+    var url = 'http://www.despegar.com.ar/cp/shop/search/C982/C31684/2017-09-12/2017-09-15/2/0/0/NA/2017-09-12/2017-09-15/2';
     browser.get(url);
   }
 };
