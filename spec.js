aeb595aab68fdeef932653cdb79b9887782599e2
Agustin Inglese <ainglese@despegar.com>
2017-08-04 11:36:53 -0300

    Update spec.js

--- spec.js
+++ spec.js
@@ -2,7 +2,7 @@
 describe('Protractor Demo App pass', function() {
 
   it('should be able to see the first cluster', function() {
-    var clusters = element.all(by.repeater('offer in tripBoard.data.hotelOffers.data.elements'));
+    var clusters = element.all(by.repeater('package in searchCtrl.packages.data.items'));
      clusters.then(function(){
        expect(clusters.first().isDisplayed()).toBe(true);
      });
